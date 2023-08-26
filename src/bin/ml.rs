6e7f735f3bfa8a9b6aada32b3a0952b92ef7aff6
Travis Kaufman <travis.kaufman@gmail.com>
2023-08-26 01:35:36 -0400

    Meatloaf

--- src/bin/ml.rs
+++ src/bin/ml.rs
@@ -7,21 +7,29 @@ struct Cli {
     run: Vec<String>,
 }
 
+fn say_hello() {
+    let rainbow = rainbow_text::Rainbow::default();
+    let _ = rainbow.write("🟫 MEATLOAF v0\n");
+    println!();
+    println!("To get started, try running a command, like:");
+    println!("ml echo 'Hello, world!'");
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    let cmd = cli.run.join(" ");
+    if cmd.is_empty() {
+        say_hello();
+        return;
+    }
+
     let context = zmq::Context::new();
     let requester = context.socket(zmq::REQ).unwrap();
-
+    let mut msg = zmq::Message::new();
     assert!(requester.connect("tcp://localhost:5555").is_ok());
 
-    let cli = Cli::parse();
-    let mut msg = zmq::Message::new();
-
-    let cmd = match cli.run.join(" ") {
-        c if !c.is_empty() => c,
-        _ => String::from("echo 'Hello, World!'"),
-    };
     requester.send(cmd.as_str(), 0).expect("Send failed");
-
     requester.recv(&mut msg, 0).expect("recv failed :(");
     println!("{}", msg.as_str().unwrap_or(""));
 }
